3a2709087f117d4113347115fce6b6c9a75e621b
Daniel Redly <dredly@gmail.com>
2023-12-04 23:30:32 +0000

    Day 2 part 1

---
+++ src/day2.rs
@@ -0,0 +1,41 @@
+use std::{collections::HashMap, fs::File, io::Read};
+
+use regex::Regex;
+
+pub fn part1_answer() {
+    let answer = sum_of_possible_game_ids("./inputs/day2/part1.txt".to_string());
+    println!("Answer for day 2 part 1 = {}", answer);
+}
+
+fn sum_of_possible_game_ids(input_file_path: String) -> u32 {
+    let mut file = File::open(input_file_path).expect("Failed to open input file");
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Unable to read file");
+
+    let max_amounts: HashMap<&str, u32> = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+
+    let re = Regex::new(r"(\d+)\s(green|red|blue)").unwrap();
+
+    contents.split("\n")
+        .map(|line| possibility_value(line, &max_amounts, &re))
+        .sum()
+}
+
+fn possibility_value(game: &str, max_amounts: &HashMap<&str, u32>, amount_regex: &Regex) -> u32 {
+    // Returns the id of a game if it is possible, otherwise returns 0
+    let spl: Vec<&str> = game.split(":").collect();
+    let amounts = spl[1];
+    for (_, [amount, colour]) in amount_regex.captures_iter(amounts).map(|c| c.extract()) {
+        if max_amounts.get(colour).unwrap() < &amount.parse::<u32>().unwrap() {
+            return 0;
+        }
+    }
+    let game_info = spl[0];
+    let game_id = game_info.split(" ").nth(1).unwrap().parse::<u32>().unwrap();
+    return game_id
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 pub mod day1;
+pub mod day2;
 
 fn main() {
     day1::part1_answer();
     day1::part2_answer();
+
+    day2::part1_answer();
 }
